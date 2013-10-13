e7332111e0587bcd76047da6fb5de93f4ff87bad
Torben Muldvang Andersen <muldvang@gmail.com>
2013-10-13 12:50:58 +0200

    Syntax error.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,7 +14,7 @@
 (setq package-archives '(("elpa" . "http://tromey.com/elpa/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")
-                         ("gnu" . "http://elpa.gnu.org/packages/"))
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
 
 ;; Set the font
 (set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 95)
