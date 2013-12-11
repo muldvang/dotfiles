026a80fe2c69b910814c77f22aad29a49f800544
Torben Muldvang Andersen <muldvang@gmail.com>
2013-12-11 22:17:29 +0100

    It seem that Emacs updated this file automatically.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -18,11 +18,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-preview ((t (:foreground "wheat"))))
- '(company-preview-common ((t (:foreground "dark gray"))))
+ '(company-preview ((t (:foreground "wheat"))) t)
+ '(company-preview-common ((t (:foreground "dark gray"))) t)
  '(highlight ((t (:background "grey12"))))
  '(hl-line ((t (:background "#393f3f"))))
- '(ido-indicator ((t (:background "red" :foreground "yellow1" :width condensed))))
- '(ido-only-match ((t (:foreground "#8ae234"))))
- '(ido-subdir ((t (:foreground "#729fcf"))))
- '(proof-locked-face ((t (:background "dark slate gray")))))
+ '(ido-indicator ((t (:background "red" :foreground "yellow1" :width condensed))) t)
+ '(ido-only-match ((t (:foreground "#8ae234"))) t)
+ '(ido-subdir ((t (:foreground "#729fcf"))) t)
+ '(proof-locked-face ((t (:background "dark slate gray"))) t))
