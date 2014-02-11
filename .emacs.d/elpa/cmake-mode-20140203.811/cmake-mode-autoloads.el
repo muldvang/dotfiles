25272db4efe86b86318165e22272630c90b34821
Torben Muldvang Andersen <muldvang@gmail.com>
2014-02-11 21:23:27 +0100

    ELPA update

---
+++ .emacs.d/elpa/cmake-mode-20140203.811/cmake-mode-autoloads.el
@@ -0,0 +1,47 @@
+;;; cmake-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "cmake-mode" "cmake-mode.el" (21241 9869 711324
+;;;;;;  110000))
+;;; Generated autoloads from cmake-mode.el
+
+(autoload 'cmake-mode "cmake-mode" "\
+Major mode for editing CMake listfiles.
+
+\(fn)" t nil)
+
+(autoload 'cmake-command-run "cmake-mode" "\
+Runs the command cmake with the arguments specified.  The
+optional argument topic will be appended to the argument list.
+
+\(fn TYPE &optional TOPIC BUFFER)" t nil)
+
+(autoload 'cmake-help-list-commands "cmake-mode" "\
+Prints out a list of the cmake commands.
+
+\(fn)" t nil)
+
+(autoload 'cmake-get-command "cmake-mode" "\
+Gets the topic from the minibuffer input.  The default is the word the cursor is on.
+
+\(fn)" nil nil)
+
+(autoload 'cmake-help-command "cmake-mode" "\
+Prints out the help message corresponding to the command the cursor is on.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-mode))
+
+(add-to-list 'auto-mode-alist '("\\.cmake\\'" . cmake-mode))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; cmake-mode-autoloads.el ends here
--- .emacs.d/elpa/haskell-mode-20140131.347/haskell-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "haskell-mode" "20140131.347" "A Haskell editing mode" 'nil)
