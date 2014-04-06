4b52647f3330cf8956fce4ce8591b0e773df7c3c
Torben Muldvang Andersen <muldvang@gmail.com>
2014-04-06 18:05:45 +0200

    Installed lua-mode.

--- .emacs.d/el-get/.status.el
+++ .emacs.d/el-get/.status.el
@@ -1,110 +1,112 @@
 ((ac-math status "installed" recipe
-	  (:name ac-math :type http :website "http://code.google.com/p/ac-math/" :description "This is an add-on which defines three ac-sources for the auto-complete package" :url "https://ac-math.googlecode.com/svn/trunk/ac-math.el"))
+          (:name ac-math :type http :website "http://code.google.com/p/ac-math/" :description "This is an add-on which defines three ac-sources for the auto-complete package" :url "https://ac-math.googlecode.com/svn/trunk/ac-math.el"))
  (auctex status "installed" recipe
-	 (:name auctex :website "http://www.gnu.org/software/auctex/" :description "AUCTeX is an extensible package for writing and formatting TeX files in GNU Emacs and XEmacs. It supports many different TeX macro packages, including AMS-TeX, LaTeX, Texinfo, ConTeXt, and docTeX (dtx files)." :type git :module "auctex" :url "git://git.savannah.gnu.org/auctex.git" :build
-		`(("./autogen.sh")
-		  ("./configure" "--without-texmf-dir" "--with-packagelispdir=$(pwd)" "--with-packagedatadir=$(pwd)" ,(cond
-														       ((eq system-type 'darwin)
-															"--with-lispdir=`pwd`")
-														       (t ""))
-		   ,(concat "--with-emacs=" el-get-emacs))
-		  "make")
-		:load-path
-		("." "preview")
-		:load
-		("tex-site.el" "preview/preview-latex.el")
-		:info "doc"))
+         (:name auctex :website "http://www.gnu.org/software/auctex/" :description "AUCTeX is an extensible package for writing and formatting TeX files in GNU Emacs and XEmacs. It supports many different TeX macro packages, including AMS-TeX, LaTeX, Texinfo, ConTeXt, and docTeX (dtx files)." :type git :module "auctex" :url "git://git.savannah.gnu.org/auctex.git" :build
+                `(("./autogen.sh")
+                  ("./configure" "--without-texmf-dir" "--with-packagelispdir=$(pwd)" "--with-packagedatadir=$(pwd)" ,(cond
+                                                                                                                       ((eq system-type 'darwin)
+                                                                                                                        "--with-lispdir=`pwd`")
+                                                                                                                       (t ""))
+                   ,(concat "--with-emacs=" el-get-emacs))
+                  "make")
+                :load-path
+                ("." "preview")
+                :load
+                ("tex-site.el" "preview/preview-latex.el")
+                :info "doc"))
  (auto-complete status "installed" recipe
-		(:name auto-complete :website "https://github.com/auto-complete/auto-complete" :description "The most intelligent auto-completion extension." :type github :pkgname "auto-complete/auto-complete" :depends
-		       (popup fuzzy)))
+                (:name auto-complete :website "https://github.com/auto-complete/auto-complete" :description "The most intelligent auto-completion extension." :type github :pkgname "auto-complete/auto-complete" :depends
+                       (popup fuzzy)))
  (cl-lib status "installed" recipe
-	 (:name cl-lib :builtin "24.3" :type elpa :description "Properly prefixed CL functions and macros" :url "http://elpa.gnu.org/packages/cl-lib.html"))
+         (:name cl-lib :builtin "24.3" :type elpa :description "Properly prefixed CL functions and macros" :url "http://elpa.gnu.org/packages/cl-lib.html"))
  (cmake-mode status "installed" recipe
-	     (:name cmake-mode :website "http://www.itk.org/Wiki/CMake/Editors/Emacs" :description "Provides syntax highlighting and indentation for CMakeLists.txt and *.cmake source files." :type http :url "http://cmake.org/gitweb?p=cmake.git;a=blob_plain;hb=master;f=Auxiliary/cmake-mode.el"))
+             (:name cmake-mode :website "http://www.itk.org/Wiki/CMake/Editors/Emacs" :description "Provides syntax highlighting and indentation for CMakeLists.txt and *.cmake source files." :type http :url "http://cmake.org/gitweb?p=cmake.git;a=blob_plain;hb=master;f=Auxiliary/cmake-mode.el"))
  (color-theme status "installed" recipe
-	      (:name color-theme :description "An Emacs-Lisp package with more than 50 color themes for your use. For questions about color-theme" :website "http://www.nongnu.org/color-theme/" :type http-tar :options
-		     ("xzf")
-		     :url "http://download.savannah.gnu.org/releases/color-theme/color-theme-6.6.0.tar.gz" :load "color-theme.el" :features "color-theme" :post-init
-		     (progn
-		       (color-theme-initialize)
-		       (setq color-theme-is-global t))))
+              (:name color-theme :description "An Emacs-Lisp package with more than 50 color themes for your use. For questions about color-theme" :website "http://www.nongnu.org/color-theme/" :type http-tar :options
+                     ("xzf")
+                     :url "http://download.savannah.gnu.org/releases/color-theme/color-theme-6.6.0.tar.gz" :load "color-theme.el" :features "color-theme" :post-init
+                     (progn
+                       (color-theme-initialize)
+                       (setq color-theme-is-global t))))
  (color-theme-tangotango status "installed" recipe
-			 (:name color-theme-tangotango :description "Another color theme based on the Tango palette." :type github :pkgname "juba/color-theme-tangotango" :depends color-theme :prepare
-				(autoload 'color-theme-tangotango "color-theme-tangotango" "color-theme: tangotango" t)))
+                         (:name color-theme-tangotango :description "Another color theme based on the Tango palette." :type github :pkgname "juba/color-theme-tangotango" :depends color-theme :prepare
+                                (autoload 'color-theme-tangotango "color-theme-tangotango" "color-theme: tangotango" t)))
  (dash status "installed" recipe
        (:name dash :description "A modern list api for Emacs. No 'cl required." :type github :pkgname "magnars/dash.el"))
  (deferred status "installed" recipe
    (:name deferred :description "Simple asynchronous functions for emacs lisp" :website "https://github.com/kiwanami/emacs-deferred" :type github :pkgname "kiwanami/emacs-deferred" :features "deferred"))
  (diff-hl status "installed" recipe
-	  (:name diff-hl :type github :pkgname "dgutov/diff-hl" :description "Highlights uncommited changes in fringe" :prepare
-		 (add-to-list 'custom-theme-load-path default-directory)))
+          (:name diff-hl :type github :pkgname "dgutov/diff-hl" :description "Highlights uncommited changes in fringe" :prepare
+                 (add-to-list 'custom-theme-load-path default-directory)))
  (el-get status "installed" recipe
-	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "master" :pkgname "dimitri/el-get" :info "." :compile
-		("el-get.*\\.el$" "methods/")
-		:load "el-get.el"))
+         (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "master" :pkgname "dimitri/el-get" :info "." :compile
+                ("el-get.*\\.el$" "methods/")
+                :load "el-get.el"))
  (epl status "installed" recipe
       (:name epl :description "EPL provides a convenient high-level API for various package.el versions, and aims to overcome its most striking idiocies." :type github :pkgname "cask/epl"))
  (expand-region status "installed" recipe
-		(:name expand-region :type github :pkgname "magnars/expand-region.el" :description "Expand region increases the selected region by semantic units. Just keep pressing the key until it selects what you want." :website "https://github.com/magnars/expand-region.el#readme" :features expand-region))
+                (:name expand-region :type github :pkgname "magnars/expand-region.el" :description "Expand region increases the selected region by semantic units. Just keep pressing the key until it selects what you want." :website "https://github.com/magnars/expand-region.el#readme" :features expand-region))
  (f status "installed" recipe
     (:name f :website "https://github.com/rejeep/f.el" :description "Modern API for working with files and directories in Emacs" :depends
-	   (s dash)
-	   :type github :pkgname "rejeep/f.el"))
+           (s dash)
+           :type github :pkgname "rejeep/f.el"))
  (fill-column-indicator status "installed" recipe
-			(:name fill-column-indicator :type github :website "https://github.com/alpaker/Fill-Column-Indicator#readme" :description "An Emacs minor mode that graphically indicates the fill column." :pkgname "alpaker/Fill-Column-Indicator" :features fill-column-indicator))
+                        (:name fill-column-indicator :type github :website "https://github.com/alpaker/Fill-Column-Indicator#readme" :description "An Emacs minor mode that graphically indicates the fill column." :pkgname "alpaker/Fill-Column-Indicator" :features fill-column-indicator))
  (flycheck status "installed" recipe
-	   (:name flycheck :type github :pkgname "flycheck/flycheck" :description "On-the-fly syntax checking extension" :build
-		  '(("makeinfo" "-o" "doc/flycheck.info" "doc/flycheck.texi"))
-		  :info "./doc" :depends
-		  (s dash cl-lib f pkg-info)))
+           (:name flycheck :type github :pkgname "flycheck/flycheck" :description "On-the-fly syntax checking extension" :build
+                  '(("makeinfo" "-o" "doc/flycheck.info" "doc/flycheck.texi"))
+                  :info "./doc" :depends
+                  (s dash cl-lib f pkg-info)))
  (flycheck-color-mode-line status "installed" recipe
-			   (:name flycheck-color-mode-line :type github :pkgname "flycheck/flycheck-color-mode-line" :depends
-				  (flycheck)))
+                           (:name flycheck-color-mode-line :type github :pkgname "flycheck/flycheck-color-mode-line" :depends
+                                  (flycheck)))
  (fuzzy status "installed" recipe
-	(:name fuzzy :website "https://github.com/auto-complete/fuzzy-el" :description "Fuzzy matching utilities for GNU Emacs" :type github :pkgname "auto-complete/fuzzy-el"))
+        (:name fuzzy :website "https://github.com/auto-complete/fuzzy-el" :description "Fuzzy matching utilities for GNU Emacs" :type github :pkgname "auto-complete/fuzzy-el"))
  (haskell-mode status "installed" recipe
-	       (:name haskell-mode :description "A Haskell editing mode" :type github :pkgname "haskell/haskell-mode" :info "." :build
-		      `(("make" ,(format "EMACS=%s" el-get-emacs)
-			 "all"))
-		      :post-init
-		      (progn
-			(require 'haskell-mode-autoloads)
-			(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-			(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation))))
+               (:name haskell-mode :description "A Haskell editing mode" :type github :pkgname "haskell/haskell-mode" :info "." :build
+                      `(("make" ,(format "EMACS=%s" el-get-emacs)
+                         "all"))
+                      :post-init
+                      (progn
+                        (require 'haskell-mode-autoloads)
+                        (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+                        (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation))))
  (hungry-delete status "installed" recipe
-		(:name hungry-delete :description "Enables hungry deletion in all modes." :type github :pkgname "nflath/hungry-delete"))
+                (:name hungry-delete :description "Enables hungry deletion in all modes." :type github :pkgname "nflath/hungry-delete"))
  (irony-mode status "installed" recipe
-	     (:name irony-mode :description "Emacs package using Clang for C & C++ completion, on-the-fly syntax checking and more!" :depends
-		    (yasnippet auto-complete json)
-		    :type github :branch "develop" :pkgname "Sarcasm/irony-mode" :build
-		    (("mkdir -p build")
-		     ("(cd build && cmake -DCMAKE_BUILD_TYPE=Release ..)")
-		     ("make -j5 -C build install"))
-		    :compile
-		    ("elisp/" "elisp/irony/")
-		    :load-path "elisp"))
+             (:name irony-mode :description "Emacs package using Clang for C & C++ completion, on-the-fly syntax checking and more!" :depends
+                    (yasnippet auto-complete json)
+                    :type github :branch "develop" :pkgname "Sarcasm/irony-mode" :build
+                    (("mkdir -p build")
+                     ("(cd build && cmake -DCMAKE_BUILD_TYPE=Release ..)")
+                     ("make -j5 -C build install"))
+                    :compile
+                    ("elisp/" "elisp/irony/")
+                    :load-path "elisp"))
  (json status "installed" recipe
        (:name json :description "JavaScript Object Notation parser / generator" :type http :builtin "23" :url "http://edward.oconnor.cx/elisp/json.el" :features json))
+ (lua-mode status "installed" recipe
+           (:name lua-mode :description "A major-mode for editing Lua scripts" :website "https://github.com/immerrr/lua-mode" :type git :url "https://github.com/immerrr/lua-mode"))
  (pkg-info status "installed" recipe
-	   (:name pkg-info :description "Provide information about Emacs packages." :type github :pkgname "lunaryorn/pkg-info.el" :depends
-		  (dash epl)))
+           (:name pkg-info :description "Provide information about Emacs packages." :type github :pkgname "lunaryorn/pkg-info.el" :depends
+                  (dash epl)))
  (pkgbuild-mode status "installed" recipe
-		(:name pkgbuild-mode :description "Major mode for editing PKGBUILD files" :type github :pkgname "juergenhoetzel/pkgbuild-mode" :features pkgbuild-mode :post-init
-		       (add-to-list 'auto-mode-alist
-				    '("PKGBUILD$" . pkgbuild-mode))))
+                (:name pkgbuild-mode :description "Major mode for editing PKGBUILD files" :type github :pkgname "juergenhoetzel/pkgbuild-mode" :features pkgbuild-mode :post-init
+                       (add-to-list 'auto-mode-alist
+                                    '("PKGBUILD$" . pkgbuild-mode))))
  (popup status "installed" recipe
-	(:name popup :website "https://github.com/auto-complete/popup-el" :description "Visual Popup Interface Library for Emacs" :type github :submodule nil :pkgname "auto-complete/popup-el"))
+        (:name popup :website "https://github.com/auto-complete/popup-el" :description "Visual Popup Interface Library for Emacs" :type github :submodule nil :pkgname "auto-complete/popup-el"))
  (powerline status "installed" recipe
-	    (:name powerline :website "https://github.com/milkypostman/powerline" :depends
-		   (cl-lib)
-		   :description "Powerline for Emacs" :type github :pkgname "milkypostman/powerline" :load-path "." :features powerline))
+            (:name powerline :website "https://github.com/milkypostman/powerline" :depends
+                   (cl-lib)
+                   :description "Powerline for Emacs" :type github :pkgname "milkypostman/powerline" :load-path "." :features powerline))
  (rainbow-delimiters status "installed" recipe
-		     (:name rainbow-delimiters :website "https://github.com/jlr/rainbow-delimiters#readme" :description "Color nested parentheses, brackets, and braces according to their depth." :type github :pkgname "jlr/rainbow-delimiters"))
+                     (:name rainbow-delimiters :website "https://github.com/jlr/rainbow-delimiters#readme" :description "Color nested parentheses, brackets, and braces according to their depth." :type github :pkgname "jlr/rainbow-delimiters"))
  (s status "installed" recipe
     (:name s :description "The long lost Emacs string manipulation library." :type github :pkgname "magnars/s.el" :features s))
  (smex status "installed" recipe
        (:name smex :description "M-x interface with Ido-style fuzzy matching." :type github :pkgname "nonsequitur/smex" :features smex :post-init
-	      (smex-initialize)))
+              (smex-initialize)))
  (yasnippet status "installed" recipe
-	    (:name yasnippet :website "https://github.com/capitaomorte/yasnippet.git" :description "YASnippet is a template system for Emacs." :type github :pkgname "capitaomorte/yasnippet" :compile "yasnippet.el" :submodule nil :build
-		   (("git" "submodule" "update" "--init" "--" "snippets")))))
+            (:name yasnippet :website "https://github.com/capitaomorte/yasnippet.git" :description "YASnippet is a template system for Emacs." :type github :pkgname "capitaomorte/yasnippet" :compile "yasnippet.el" :submodule nil :build
+                   (("git" "submodule" "update" "--init" "--" "snippets")))))
