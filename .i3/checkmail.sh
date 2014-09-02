d0b08b5ab65acc0dd6ad21c332389d997306e6db
Torben Muldvang Andersen <muldvang@gmail.com>
2014-09-02 22:43:46 +0200

    Even larger timeout.

--- .i3/checkmail.sh
+++ .i3/checkmail.sh
@@ -1,1 +1,1 @@
-curl --connect-timeout 2 -m 6 -fsn https://mail.google.com/mail/feed/atom | awk -F 'fullcount>' '{print $2}' | cut -d "<" -f 1
+curl --connect-timeout 3 -m 9 -fsn https://mail.google.com/mail/feed/atom | awk -F 'fullcount>' '{print $2}' | cut -d "<" -f 1
