9ea081a4d01f8762de5c8591a368da95231a5b56
Torben Muldvang Andersen <muldvang@gmail.com>
2014-09-02 22:45:04 +0200

    Temporary fix.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -149,3 +149,24 @@ function man
     env LESS_TERMCAP_md=(set_color --bold blue) \
     man $argv
 end
+
+# Temporary fix for the "set_color: Expected an argument" bug.
+set fish_color_autosuggestion '666'  'green'
+set fish_color_command afffff
+set fish_color_comment red
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_error 'red'  '--bold'
+set fish_color_escape cyan
+set fish_color_history_current cyan
+set fish_color_host '-o'  'cyan'
+set fish_color_match cyan
+set fish_color_normal normal
+set fish_color_operator cyan
+set fish_color_param '00afff'  'cyan'
+set fish_color_quote brown
+set fish_color_redirection normal
+set fish_color_search_match --background=purple
+set fish_color_status red
+set fish_color_user '-o'  'green'
+set fish_color_valid_path --underline
