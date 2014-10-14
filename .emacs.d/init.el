819ba51633828b26a9184d4a04c0fd8678795f4e
Torben Muldvang Andersen <muldvang@gmail.com>
2014-10-14 23:14:59 +0200

    ido-ubiquitos to use ido everywhere. smex as alternative to M-x.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -203,32 +203,13 @@
 ;; Set the major mode of the scratch buffer to org-mode.
 (setq initial-major-mode 'org-mode)
 
+;; Ido
 (ido-mode t)
-(defvar ido-enable-replace-completing-read t
-  "If t, use ido-completing-read instead of completing-read if possible.
+(require 'ido-ubiquitous)
+(ido-ubiquitous-mode 1)
 
-    Set it to nil using let in around-advice for functions where the
-    original completing-read is required.  For example, if a function
-    foo absolutely must use the original completing-read, define some
-    advice like this:
-
-    (defadvice foo (around original-completing-read-only activate)
-      (let (ido-enable-replace-completing-read) ad-do-it))")
-
-;; Replace completing-read wherever possible, unless directed otherwise
-(defadvice completing-read
-    (around use-ido-when-possible activate)
-  (if (or (not ido-enable-replace-completing-read) ; Manual override disable ido
-          (and (boundp 'ido-cur-list)
-               ido-cur-list)) ; Avoid infinite loop from ido calling this
-      ad-do-it
-    (let ((allcomp (all-completions "" collection predicate)))
-      (if allcomp
-          (setq ad-return-value
-                (ido-completing-read prompt
-                                     allcomp
-                                     nil require-match initial-input hist def))
-        ad-do-it))))
+;; Smex
+(global-set-key (kbd "M-x") 'smex)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Minor mode settings
