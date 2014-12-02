0f2d4dd617dbafa8cf26834f19e0b30397d5c578
Torben Muldvang Andersen <muldvang@gmail.com>
2014-12-02 18:50:30 +0100

    Fish config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_hide_untrackedfiles 1
 
-set -g __fish_git_prompt_color_branch magenta bold
+set -g __fish_git_prompt_color_branch magenta
 set -g __fish_git_prompt_showupstream "informative"
 set -g __fish_git_prompt_char_upstream_ahead "↑"
 set -g __fish_git_prompt_char_upstream_behind "↓"
@@ -18,7 +18,7 @@ set -g __fish_git_prompt_color_dirtystate blue
 set -g __fish_git_prompt_color_stagedstate yellow
 set -g __fish_git_prompt_color_invalidstate red
 set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-set -g __fish_git_prompt_color_cleanstate green bold
+set -g __fish_git_prompt_color_cleanstate green
 
 
 # Left prompt customization
@@ -140,9 +140,6 @@ set -x MANWIDTH 80
 
 # Disable the greeting
 set fish_greeting ""
-
-# Environment variables
-set -x EDITOR 'emacsclient -t -a=""'
 
 # Syntax highlighting in less though source-highlight
 set -x LESSOPEN "| /usr/bin/src-hilite-lesspipe.sh %s"
