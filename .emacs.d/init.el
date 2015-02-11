79d9c9cf95c9bcdc8428ee330239cfee986fa85a
Torben Muldvang Andersen <muldvang@gmail.com>
2015-02-11 18:40:39 +0100

    Removed duplicate code.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -319,24 +319,6 @@
 (add-hook 'c-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook (lambda () (local-set-key "\C-y" 'yank-and-indent)))
 
-;; replace the `completion-at-point' and `complete-symbol' bindings in
-;; irony-mode's buffers by irony-mode's function
-(defun my-irony-mode-hook ()
-  (define-key irony-mode-map [remap completion-at-point]
-    'irony-completion-at-point-async)
-  (define-key irony-mode-map [remap complete-symbol]
-    'irony-completion-at-point-async))
-(add-hook 'irony-mode-hook 'my-irony-mode-hook)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-
-(eval-after-load 'company
-  '(add-to-list 'company-backends 'company-irony))
-
-;; (optional) adds CC special commands to `company-begin-commands' in order to
-;; trigger completion at interesting places, such as after scope operator
-;;     std::|
-(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
-
 ;; C++
 (add-hook 'c++-mode-hook 'fci-mode)
 (add-hook 'c++-mode-hook (lambda () (setq c-basic-offset 4)))
