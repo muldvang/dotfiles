1a8db40cd9def6585eab5c571e49a19564cfa63b
Torben Muldvang Andersen <muldvang@gmail.com>
2015-06-02 22:55:01 +0200

    Emacs configuration.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -30,8 +30,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(company-preview ((t (:foreground "wheat"))))
- ;; '(company-preview-common ((t (:foreground "dark gray"))))
  '(flycheck-color-mode-line-error-face ((t (:inherit flycheck-fringe-error :background "#806065" :foreground "#efefef" :weight normal))))
  '(flycheck-color-mode-line-info-face ((t (:inherit flycheck-fringe-info :background "#008000" :foreground "#efefef" :weight normal))))
  '(flycheck-color-mode-line-warning-face ((t (:inherit flycheck-fringe-warning :background "#804600" :foreground "#efefef" :weight normal))))
