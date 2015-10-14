5f5106dc9c90cd405a49312ecda13a2a2952bacf
Torben Muldvang Andersen <muldvang@gmail.com>
2015-10-14 16:57:24 +0200

    Fish config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,10 +9,13 @@ set -g __fish_git_prompt_char_upstream_behind "↓"
 set -g __fish_git_prompt_char_upstream_prefix ""
 
 set -g __fish_git_prompt_char_stagedstate "●"
-set -g __fish_git_prompt_char_dirtystate "✚"
+# set -g __fish_git_prompt_char_dirtystate "✚"
+set -g __fish_git_prompt_char_dirtystate "+"
 set -g __fish_git_prompt_char_untrackedfiles "…"
-set -g __fish_git_prompt_char_conflictedstate "✖"
-set -g __fish_git_prompt_char_cleanstate "✔"
+# set -g __fish_git_prompt_char_conflictedstate "✖"
+set -g __fish_git_prompt_char_conflictedstate "✕"
+# set -g __fish_git_prompt_char_cleanstate "✔"
+set -g __fish_git_prompt_char_cleanstate "✓"
 
 set -g __fish_git_prompt_color_dirtystate blue
 set -g __fish_git_prompt_color_stagedstate yellow
