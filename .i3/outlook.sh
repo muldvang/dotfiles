964ec7a636f9b45db434702cbb9b42786f79843a
Torben Muldvang Andersen <muldvang@gmail.com>
2015-10-18 17:53:29 +0200

    Only check outlook mail on working pc.

--- .i3/outlook.sh
+++ .i3/outlook.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
-first=$(xwininfo -root -tree | grep -P 'Outlook Web App' | grep Navigator | cut -d ' ' -f 10)
+if test $(hostname) = "laptop-110"
+then
+   first=$(xwininfo -root -tree | grep -P 'Outlook Web App' | grep Navigator | cut -d ' ' -f 10)
 
-if test "$first" = ""
-then
-    echo "Error"
-    echo "Error"
-    echo "#f92672"
-fi
+   if test "$first" = ""
+   then
+       echo "Error"
+       echo "Error"
+       echo "#f92672"
+   fi
 
-if test "$first" = "\"Torben"
-then
-    exit
-fi
+   if test "$first" = "\"Torben"
+   then
+       exit
+   fi
 
-arr=($(echo $first | tr -s [:punct:] ' '))
-mail=${arr[0]}
-cal=${arr[1]}
+   arr=($(echo $first | tr -s [:punct:] ' '))
+   mail=${arr[0]}
+   cal=${arr[1]}
 
-if test $mail -gt 0 -a $cal -gt 0
-then
-    echo "Mail and cal"
-    echo "Mail and cal"
-    echo "#f92672"
-elif test $mail -gt 0
-then
-    echo "Mail"
-elif test $cal -gt 0
-then
-    echo "Cal"
-    echo "Cal"
-    echo "#f92672"
+   if test $mail -gt 0 -a $cal -gt 0
+   then
+       echo "Mail and cal"
+       echo "Mail and cal"
+       echo "#f92672"
+   elif test $mail -gt 0
+   then
+       echo "Mail"
+   elif test $cal -gt 0
+   then
+       echo "Cal"
+       echo "Cal"
+       echo "#f92672"
+   fi
 fi
