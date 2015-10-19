41608e5958c1be0f7e38d93d366ea87c61fec53c
Torben Muldvang Andersen <muldvang@gmail.com>
2015-10-19 11:43:20 +0200

    Use menu key as compose key.

--- .i3/switch_keyboard_layout.fish
+++ .i3/switch_keyboard_layout.fish
@@ -6,5 +6,5 @@ if test "$ALT_GR" = "Control_R"
         setxkbmap -layout dk -option
 else
         echo "Making Caps Lock AltGr and swapping Ctrl and Alt."
-        setxkbmap -layout dk -option -option lv3:caps_switch -option ctrl:swap_lalt_lctl -option ctrl:rctrl_ralt -option ctrl:ralt_rctrl
+        setxkbmap -layout dk -option -option lv3:caps_switch -option ctrl:swap_lalt_lctl -option ctrl:rctrl_ralt -option ctrl:ralt_rctrl -option compose:menu
 end
