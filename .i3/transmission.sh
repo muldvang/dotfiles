a39b5fd4ce4ff7f612cce1b843c32bf53e7bdc42
Torben Muldvang Andersen <muldvang@gmail.com>
2015-10-25 16:25:28 +0100

    Show if running transmission in turtle mode.

--- .i3/transmission.sh
+++ .i3/transmission.sh
@@ -6,5 +6,10 @@ then
     stats=$(transmission-remote -l | tail -n 1 | column -t)
     down=$(echo $stats | cut -d " " -f 5)
     up=$(echo $stats | cut -d " " -f 4)
-    echo "$count ‒ $down / $up"
+    if $(transmission-remote -si | grep 'Enabled turtle' > /dev/null)
+    then
+        echo "$count ‒ $down / $up (turtle)"
+    else
+        echo "$count ‒ $down / $up"
+    fi
 fi
