109d8937b34785a6da4ed7730f34b15cb36cb712
Torben Muldvang Andersen <muldvang@gmail.com>
2015-12-02 22:53:28 +0100

    Use systemctl to start i3lock from i3.

--- .i3/power_management.sh
+++ .i3/power_management.sh
@@ -8,7 +8,7 @@ Poweroff"
 action=$(echo "$actions" | dmenu -p 'Choose action:' -nb '#171b1c' -nf '#FFFFFF' -sb '#5D8C00' -sf '#FFFFFF' -i -fn 'sans-9' -h 18 -b)
 case $action in
     "Lock")
-        i3lock -i $(shuf -n 1 -e ~/Pictures/wallpapers/Van_Gogh/*);;
+        systemctl --user start i3lock.service;;
     "Suspend")
         systemctl suspend;;
     "Hybrid sleep")
