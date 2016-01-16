7ec68f9a0f47b0779d813286e99282c874e36444
Torben Muldvang Andersen <muldvang@gmail.com>
2016-01-16 16:33:27 +0100

    Use shift as compose key as well as normal shift key.

--- .i3/switch_keyboard_layout.fish
+++ .i3/switch_keyboard_layout.fish
@@ -5,7 +5,10 @@ set ALT_GR (xmodmap -pke | grep 108 | rev | cut -d " " -f 1 | rev)
 if test "$ALT_GR" = "Control_R"
         echo "Switching to standard layout."
         setxkbmap -layout dk -option
+        killall -q xcape
 else
         echo "Making Caps Lock AltGr and swapping Ctrl and Alt."
         setxkbmap -layout dk -option -option lv3:caps_switch -option ctrl:swap_lalt_lctl -option ctrl:rctrl_ralt -option ctrl:ralt_rctrl -option compose:menu
+        killall -q xcape
+        xcape -e 'Shift_L=Multi_key;Shift_R=Multi_key'
 end
