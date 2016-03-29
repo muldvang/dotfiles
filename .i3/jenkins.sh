88f7faaa08637e1432122d2a98c82e38b98ca054
Torben Muldvang Andersen <muldvang@gmail.com>
2016-03-29 17:08:17 +0200

    i3bar configuration.

--- .i3/jenkins.sh
+++ .i3/jenkins.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 if test $(hostname) = "laptop-110"
 then
-    color=$(curl "http://jenkins-ivd:8080/jenkins-ivd/view/Plugins%20and%20bioinformatics/api/json?pretty=true" | grep color | cut -d \" -f 4 | sort -r | uniq)
-    if test "$color" != "blue"
+    color=$(curl "http://jenkins-ivd-centos7:8080/jenkins-ivd-centos7/view/HIV/api/json?pretty=true" | grep color | cut -d \" -f 4 | sort -r | uniq | head -n 1)
+    if test "$color" = "aborted"
+    then
+        echo Aborted
+        echo Aborted
+        echo "#f92672"
+    elif test "$color" != "blue" -a "$color" != "blue_anime"
     then
         echo "Error"
         echo "Error"
--- .i3/mpd.sh
+++ .i3/mpd.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-mpc idle player update > /dev/null; mpc current
+status=$(mpc | grep -o -P '\[[a-z]*\]' | grep -o -P '[a-z]*')
+if test $status = playing
+then
+    mpc current
+elif test $status = stopped
+then
+    exit
+else
+    status=$(echo ${status:0:1} | tr  '[a-z]' '[A-Z]')${status:1}
+    echo $status
+fi
