14dac0c24b664126ec662296d14df3f498cc7035
Torben Muldvang Andersen <muldvang@gmail.com>
2016-04-12 16:57:57 +0200

    i3 config.

--- .i3/jenkins.sh
+++ .i3/jenkins.sh
@@ -9,8 +9,21 @@ then
         echo "#f92672"
     elif test "$color" != "blue" -a "$color" != "blue_anime"
     then
-        echo "Error"
-        echo "Error"
+        echo "Plugin"
+        echo "Plugin"
         echo "#A52A2A"
+    else
+        color=$(curl "http://jenkins-ivd:8080/jenkins-ivd/view/IVD%20liste/api/json?pretty=true" | grep color | cut -d \" -f 4 | sort -r | uniq | head -n 1)
+        if test "$color" = "aborted"
+        then
+            echo Aborted
+            echo Aborted
+            echo "#f92672"
+        elif test "$color" != "blue" -a "$color" != "blue_anime"
+        then
+            echo "Platform"
+            echo "Platform"
+            echo "#A52A2A"
+        fi
     fi
 fi
