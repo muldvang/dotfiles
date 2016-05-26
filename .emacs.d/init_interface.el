a60adaa21024c171a1afbb2d2de28ae9412010be
Torben Muldvang Andersen <muldvang@gmail.com>
2016-05-26 20:57:23 +0200

    Switched from el-get to package.el with use-package.

---
+++ .emacs.d/init_interface.el
@@ -0,0 +1,18 @@
+;; Graphical stuff
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(set-scroll-bar-mode nil)
+(setq frame-title-format '("" "%b - Emacs"))
+
+;; Frame stuff
+(line-number-mode t)
+(column-number-mode t)
+(setq visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow))
+(setq-default indicate-empty-lines t)
+
+;; Buffer stuff
+(load-theme 'adwaita t)
+(set-face-attribute 'default nil :family "monospace" :height 100)
+(blink-cursor-mode 0)
+(setq visible-bell t)
+(show-paren-mode t)
