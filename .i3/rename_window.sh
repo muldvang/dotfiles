b6b462c5c6e3a4ad660cbf52a6a76b625fc3983b
Torben Muldvang Andersen <muldvang@gmail.com>
2016-07-07 16:57:45 +0200

    Rename windows using xdotool, as i3-msg title_format does not work with Eclipse windows.

--- .i3/rename_window.sh
+++ .i3/rename_window.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 name=$(echo | dmenu -p 'Window name:' -nb '#ededed' -nf '#313739' -sb '#4A90D9' -sf '#313739' -i -fn 'sans-9' -h 18 -b)
-i3-msg title_format "$name"
+current_window_id=$(xdotool getactivewindow)
+xdotool set_window -name "$name" $current_window_id
