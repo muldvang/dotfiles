a9174554e988669911895b38fec3bb3f377b73dc
Torben Muldvang Andersen <muldvang@gmail.com>
2016-09-05 16:57:32 +0200

    i3 config.

--- .i3/seqflowivd_services.sh
+++ .i3/seqflowivd_services.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ps aux | grep [.]Main | tr -s ' ' | cut -d ' ' -f 14- | rev | cut -d '.' -f 2 | rev | tr '\n' ' '
+ps aux | grep -v '/home/tandersen/.local/' | grep [.]clc | tr -s ' ' | cut -d ' ' -f 14- | rev | cut -d '.' -f 2 | rev | tr '\n' ' '
