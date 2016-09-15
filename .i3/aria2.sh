392b88ed7c6ab92c1b9d5cd5f0f53dc9987789e0
Torben Muldvang Andersen <muldvang@gmail.com>
2016-09-15 19:47:42 +0200

    i3 config.

--- .i3/aria2.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/fish
-
-for id in (diana list | cut -f 1 -d " ")
-    set entry (diana files $id | tr -s " ")
-    basename (echo $entry | cut -f 4 -d " ")
-    echo $entry | cut -f 3 -d " "
-end
