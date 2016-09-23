5fc5b0229c88cec0b57d2130dcc8b4c0e1fb2c9a
Torben Muldvang Andersen <muldvang@gmail.com>
2016-09-23 11:32:07 +0200

    i3 config.

--- .i3/jenkins.sh
+++ .i3/jenkins.sh
@@ -7,7 +7,7 @@ then
         echo Aborted
         echo Aborted
         echo "#A52A2A"
-    elif test "$color" != "blue" -a "$color" != "blue_anime"
+    elif test "$color" != "blue" -a "$color" != "blue_anime" -a "$color" != "notbuilt"
     then
         echo "Plugin"
         echo "Plugin"
