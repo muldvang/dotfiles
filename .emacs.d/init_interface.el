af70208de9b78200e4ebcd9f367afd734d2a8485
Torben Muldvang Andersen <muldvang@gmail.com>
2017-01-28 21:39:47 +0100

    Custom mode line format for Emacs.

--- .emacs.d/init_interface.el
+++ .emacs.d/init_interface.el
@@ -10,6 +10,21 @@
 (setq visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow))
 (setq-default indicate-empty-lines t)
 
+(setq-default mode-line-format
+              '(
+                (:propertize " %* ")
+                (:propertize " %z ")
+                (:propertize " %b " face (:weight bold))
+                (:propertize " " face (:background "grey"))
+                (:propertize mode-name face (:background "grey"))
+                (:propertize minor-mode-alist face (:background "grey"))
+                (:propertize " " face (:background "grey"))
+
+                (:propertize " " display '(space :align-to (- right 18)))
+                (:propertize " %l:%c ")
+                (:propertize " %p ")
+               ))
+
 ;; Buffer stuff
 (load-theme 'adwaita t)
 (set-face-attribute 'default nil :family "monospace" :height 95)
