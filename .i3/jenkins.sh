140eb0ffd4ad904ac5ed276a8c5d33ab10ca65ce
Torben Muldvang Andersen <muldvang@gmail.com>
2018-02-01 17:23:13 +0100

    i3bar config

--- .i3/jenkins.sh
+++ .i3/jenkins.sh
@@ -4,8 +4,8 @@ then
     color=$(curl "http://jenkins-ivd:8080/jenkins-ivd/view/HIV/api/json?pretty=true" 2> /dev/null | grep color | cut -d \" -f 4 | sort -r | uniq | head -n 1)
     if test "$color" != "blue" -a "$color" != "blue_anime" -a "$color" != "notbuilt"
     then
-        echo
-        echo
+        echo 
+        echo 
         echo "#A52A2A"
     fi
 fi
--- .i3/seqflowivd_services.sh
+++ .i3/seqflowivd_services.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-if test ps aux | grep [.]clc
+# if ps aux | grep [.]clc > /dev/null
+# then
+#     ps aux | grep -v '/home/tandersen/.local/' | grep [.]clc | tr -s ' ' | cut -d ' ' -f 14- | rev | cut -d '.' -f 2 | rev | tr '\n' ' '
+# fi
+
+HEALTH_JSON=$(curl --connect-timeout 1 http://localhost:9089/health)
+if echo $HEALTH_JSON | grep "YELLOW" > /dev/null
 then
-    ps aux | grep -v '/home/tandersen/.local/' | grep [.]clc | tr -s ' ' | cut -d ' ' -f 14- | rev | cut -d '.' -f 2 | rev | tr '\n' ' '
+    echo 
+    echo 
+    echo "#A52A2A"
+elif echo $HEALTH_JSON | grep "GREEN" > /dev/null
+then
+    echo 
+    echo 
+    echo "#4E9A06"
 fi
