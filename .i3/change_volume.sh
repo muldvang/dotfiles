807a1e3269fa26b3c22db679ada6ebdcada93fe1
Torben Muldvang Andersen <muldvang@gmail.com>
2018-10-21 19:58:39 +0200

    Use pulseaudio for managing volume

--- .i3/change_volume.sh
+++ .i3/change_volume.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-if $(hostname) == 'laptop-110':
-then
-   ALSA_CARD=PCH
-fi
+# if $(hostname) == 'laptop-110':
+# then
+#    ALSA_CARD=PCH
+# fi
 
-if pgrep pulseaudio > /dev/null
+# if pgrep pulseaudio > /dev/null
+# then
+if test "$1" = down
 then
-    if test "$1" = down
-    then
-        amixer -q -D pulse sset Master 2%-
-        amixer -q sset Master 2%-
-    elif test "$1" = up
-    then
-        amixer -q -D pulse sset Master 2%+
-        amixer -q sset Master 2%+
-    fi
-else
-    if test "$1" = down
-    then
-        amixer -q set PCM 2%-
-    elif test "$1" = up
-    then
-        amixer -q set PCM 2%+
-    fi
+    # amixer -q -D pulse sset Master 2%- -M
+    # amixer -q sset Master 2%- -M
+    pactl set-sink-volume 0 -2dB
+elif test "$1" = up
+then
+    # amixer -q -D pulse sset Master 2%+ -M
+    # amixer -q sset Master 2%+ -M
+    pactl set-sink-volume 0 +2dB
 fi
+# else
+#     if test "$1" = down
+#     then
+#         amixer -q set Master 2%-
+#     elif test "$1" = up
+#     then
+#         amixer -q set Master 2%+
+#     fi
+# fi
