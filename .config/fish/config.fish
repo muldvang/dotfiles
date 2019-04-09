79643d7195c67c91d003f4303abb2c0ecae74f53
Torben Muldvang Andersen - QIAGEN <Torben.Andersen@qiagen.com>
2019-04-09 13:54:54 +0200

    Use ranger to change directories

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -245,3 +245,13 @@ function abduco
         set -x ABDUCO 1
         command abduco $argv
 end
+
+
+function ranger-cd
+    set tmpfile "/tmp/pwd-from-ranger"
+    ranger --choosedir=$tmpfile $argv
+    set rangerpwd (cat $tmpfile)
+    if test "$PWD" != $rangerpwd
+        cd $rangerpwd
+    end
+end
