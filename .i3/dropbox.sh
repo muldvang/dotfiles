c26bd878ac4181690e4540786fb4de99c6b66b8b
Torben Muldvang Andersen <muldvang@gmail.com>
2019-05-19 20:55:47 +0200

    Subscribe to Dropbox sync changes, avoiding polling.

--- .i3/dropbox.sh
+++ .i3/dropbox.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/bash
 
+function print_block {
+    # status=$(dropbox-cli status)
+    status=$(echo -e 'get_dropbox_status\ndone' | socat - ~/.dropbox/command_socket | grep status | cut -f 2)
 
-# status=$(dropbox-cli status)
-status=$(echo -e 'get_dropbox_status\ndone' | socat - ~/.dropbox/command_socket | grep status | cut -f 2)
+    if test "$status" = "Up to date"
+    then
+        echo ' '
+    elif echo "$status" | grep -E "Please update within the next [0-9][0-9]? days" > /dev/null
+    then
+        echo  "$status" | grep -E -o "Please update within the next [0-9][0-9]? days"
+        echo  "Update"
+    else
+        echo  "$status"
+    fi
+}
 
-if test "$status" = "Up to date"
-then
-    echo
-    exit
-elif echo "$status" | grep -E "Please update within the next [0-9][0-9]? days" > /dev/null
-then
-    echo  "$status" | grep -E -o "Please update within the next [0-9][0-9]? days"
-    echo  "Update"
-else
-    echo  "$status"
-fi
+while read -r EVENT; do
+    print_block
+done < <(socat - ~/.dropbox/iface_socket)
