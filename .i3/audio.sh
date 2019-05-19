50b2fa44b7aa8979c97292960c7fe33fdafb2546
Torben Muldvang Andersen <muldvang@gmail.com>
2019-05-19 20:05:20 +0200

    Subscribe to volume changes, avoiding polling and sending signals.

--- .i3/audio.sh
+++ .i3/audio.sh
@@ -1,41 +1,14 @@
 #!/bin/bash
 
-PAC=$(pacmd list-sinks | grep -P "\*" --after-context 36)
-VOLUME=$(echo "$PAC" | grep -P [0-9]+% -o | head -n 1 | tr -d '%')
-SINK=$(pacmd list-sinks | grep -P "\*" --after-context 1000| grep 'device.description' | head -n1 | cut -d '"' -f 2 )
-echo -n "$SINK: "
+function print_block {
+    PAC=$(pacmd list-sinks | grep -P "\*" --after-context 36)
+    VOLUME=$(echo "$PAC" | grep -P [0-9]+% -o | head -n 1 | tr -d '%')
+    SINK=$(pacmd list-sinks | grep -P "\*" --after-context 1000| grep 'device.description' | head -n1 | cut -d '"' -f 2 )
+    echo -n "$SINK: $VOLUME %"
+}
 
-SVOL=$(($VOLUME / 5))
+print_block
 
-echo -n "<span font_desc='monospace' size='4000' rise='3000' strikethrough='true'>"
-
-i=0
-while test $i -lt $SVOL
-do
-    i=$((i + 1))
-    echo -n █
-done
-
-if test $VOLUME = 100
-then
-    echo -n █
-elif test $VOLUME = 0
-then
-    echo -n ' '
-else
-    SMALL=$(($VOLUME - $SVOL * 5))
-    if test $SMALL -lt 3
-    then
-        echo -n ▌
-    else
-        echo -n █
-    fi
-fi
-
-while test $i -lt 20
-do
-    i=$((i + 1))
-    echo -n ' '
-done
-
-echo "</span>"
+while read -r EVENT; do
+    print_block
+done < <(pactl subscribe | stdbuf -oL grep "'change' on sink")
--- .i3/change_volume.sh
+++ .i3/change_volume.sh
@@ -11,12 +11,12 @@ if test "$1" = down
 then
     # amixer -q -D pulse sset Master 2%- -M
     # amixer -q sset Master 2%- -M
-    pactl set-sink-volume 0 -2dB
+    pactl set-sink-volume 0 -2%
 elif test "$1" = up
 then
     # amixer -q -D pulse sset Master 2%+ -M
     # amixer -q sset Master 2%+ -M
-    pactl set-sink-volume 0 +2dB
+    pactl set-sink-volume 0 +2%
 fi
 # else
 #     if test "$1" = down
