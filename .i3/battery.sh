b13ba6afda84a37aa18188578d6e6b9655f87a87
Torben Muldvang Andersen <muldvang@gmail.com>
2019-05-21 21:22:38 +0200

    Use udev events for triggering update of battery notification.

--- .i3/battery.sh
+++ .i3/battery.sh
@@ -1,53 +1,64 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-
-ACPI=$(acpi -b 2>&1 | tail -n 1)
-if test "$no_support" != ""
-then
-    exit
-fi
-
-full=$(echo $ACPI | grep -P "Unknown|Full|99%|zero")
-
-if test "$full" != ""
-then
-   exit
-fi
-
-pct=$(cat /sys/class/power_supply/BAT0/capacity)
-state=$(cat /sys/class/power_supply/BAT0/status)
-
-if test $pct -lt 10
-then
-    if test "$state" == "Discharging"
+function print_block {
+    echo 'print block'
+    ACPI=$(acpi -b 2>&1 | tail -n 1)
+    if test "$no_support" != ""
     then
-        hours=$(echo $ACPI | cut -d " " -f 5 | cut -d ":" -f 1)
-        minutes=$(echo $ACPI | cut -d " " -f 5 | cut -d ":" -f 2)
-        echo  $hours:$minutes
-        echo 
-        echo "#A52A2A"
-    else
-        echo 
-        echo 
+        return
     fi
-elif test $pct -lt 30
-then
-    echo 
-    echo 
-elif test $pct -lt 60
-then
-    echo 
-    echo 
-elif test $pct -lt 90
-then
-    echo 
-    echo 
-else
-    echo 
-    echo 
-fi
 
-if test $state == "Charging"
-then
-    echo "#4E9A06"
-fi
+    full=$(echo $ACPI | grep -P "Full|99%|zero")
+
+    if test "$full" != ""
+    then
+        return
+    fi
+
+    pct=$(cat /sys/class/power_supply/BAT0/capacity)
+    state=$(cat /sys/class/power_supply/BAT0/status)
+
+    if test $pct -lt 10
+    then
+        if test "$state" == "Discharging"
+        then
+            hours=$(echo $ACPI | cut -d " " -f 5 | cut -d ":" -f 1)
+            minutes=$(echo $ACPI | cut -d " " -f 5 | cut -d ":" -f 2)
+            echo  $hours:$minutes
+            echo 
+            echo "#A52A2A"
+        else
+            echo 
+            echo 
+        fi
+    elif test $pct -lt 30
+    then
+        echo 
+        echo 
+    elif test $pct -lt 60
+    then
+        echo 
+        echo 
+    elif test $pct -lt 90
+    then
+        echo 
+        echo 
+    else
+        echo 
+        echo 
+    fi
+
+    if test $state == "Charging"
+    then
+        echo "#4E9A06"
+    fi
+}
+
+while true
+do
+    print_block
+    while read -r EVENT
+    do
+        print_block
+    done < <(timeout 30s udevadm monitor | stdbuf -oL grep "power_supply")
+done
